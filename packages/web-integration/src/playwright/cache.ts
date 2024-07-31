37448614a532312a471021586f599cdc280b53a1
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2024-07-31 10:18:25 +0800

    chore: Added cache version determination

--- packages/web-integration/src/playwright/cache.ts
+++ packages/web-integration/src/playwright/cache.ts
@@ -1,14 +1,17 @@
 import { join } from 'path';
 import fs from 'fs';
-import { writeDumpFile, getDumpDirPath } from '@midscene/core/utils';
+import { writeDumpFile, getDumpDirPath, getPkgInfo } from '@midscene/core/utils';
 import { AiTaskCache } from '@/common/task-cache';
 
 export function writeTestCache(taskFile: string, taskTitle: string, taskCacheJson: AiTaskCache) {
+  const pkgInfo = getPkgInfo();
   writeDumpFile({
     fileName: `${taskFile}(${taskTitle})`,
     fileExt: 'json',
     fileContent: JSON.stringify(
       {
+        pkgName: pkgInfo.name,
+        pkgVersion: pkgInfo.version,
         taskFile,
         taskTitle,
         ...taskCacheJson,
@@ -22,10 +25,14 @@ export function writeTestCache(taskFile: string, taskTitle: string, taskCacheJso
 
 export function readTestCache(taskFile: string, taskTitle: string) {
   const cacheFile = join(getDumpDirPath('cache'), `${taskFile}(${taskTitle}).json`);
+  const pkgInfo = getPkgInfo();
   if (process.env.MIDSCENE_CACHE === 'true' && fs.existsSync(cacheFile)) {
     try {
       const data = fs.readFileSync(cacheFile, 'utf8');
       const jsonData = JSON.parse(data);
+      if (jsonData.pkgName !== pkgInfo.name || jsonData.pkgVersion !== pkgInfo.version) {
+        return undefined;
+      }
       return jsonData as AiTaskCache;
     } catch (err) {
       return undefined;
