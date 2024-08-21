4dea5f7a31b5e2aa480edd622f19bcd41a6f09a2
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2024-08-21 16:48:20 +0800

    chore: optimize generate code

--- packages/web-integration/tests/unit-test/extractor.test.ts
+++ packages/web-integration/tests/unit-test/extractor.test.ts
@@ -1,16 +1,31 @@
-import { join } from 'node:path';
+import path, { join } from 'node:path';
 import { parseContextFromWebPage } from '@/common/utils';
+import { generateTestData } from 'tests/ai/e2e/tool';
 import { describe, expect, it } from 'vitest';
 import { launchPage } from '../ai/puppeteer/utils';
 
 const pagePath = join(__dirname, './fixtures/extractor.html');
+
 describe(
   'extractor',
   () => {
     it('basic', async () => {
       const page = await launchPage(`file://${pagePath}`);
 
-      const { content } = await parseContextFromWebPage(page);
+      const { content, screenshotBase64 } = await parseContextFromWebPage(page);
+      await generateTestData(
+        page,
+        path.join(__dirname, 'extractor'),
+        screenshotBase64,
+        {
+          disableInputImage: true,
+          disableOutputImage: false,
+          disableOutputWithoutTextImg: true,
+          disableResizeOutputImg: true,
+          disableSnapshot: true,
+        },
+      );
+
       const list = content.map((item) => {
         return {
           content: item.content,
