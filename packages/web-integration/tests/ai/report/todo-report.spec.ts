6079547f06d6e43d1086e18dd1548dfdc7365f2f
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2024-08-22 18:24:48 +0800

    chore: add ai test

---
+++ packages/web-integration/tests/ai/report/todo-report.spec.ts
@@ -0,0 +1,27 @@
+import path from 'node:path';
+import { expect } from 'playwright/test';
+import { test } from '../e2e/fixture';
+import { getLastModifiedHTMLFile } from '../e2e/util';
+
+test('ai report', async ({ page, ai, aiAssert }, testInfo) => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const htmlFile = getLastModifiedHTMLFile(
+    path.join(__dirname, '../../../midscene_run/report'),
+  );
+  console.log('report html path:', htmlFile);
+  await page.goto(`file:${htmlFile}`);
+  await ai(
+    'Move your mouse over the top task list (next to the logo) and click Select ai todo from the drop-down list',
+  );
+  const actionsList = await ai(
+    'Sidebar task list Array<{title: string, actions: Array<string>}>',
+    {
+      type: 'query',
+    },
+  );
+  const parseList = JSON.stringify(actionsList, null, 4);
+  expect(parseList).toMatchSnapshot();
+  await aiAssert(
+    'On the left taskbar, check whether the specific execution content of the right task is normal',
+  );
+});
