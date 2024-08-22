77621bc7bdfcccd87b57fc31dead35d953360a78
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2024-08-22 17:25:47 +0800

    chore: fix e2e test

--- packages/web-integration/tests/ai/e2e/ai-auto-todo.spec.ts
+++ packages/web-integration/tests/ai/e2e/ai-auto-todo.spec.ts
@@ -4,12 +4,12 @@ import { expect } from 'playwright/test';
 import { test } from './fixture';
 import { getLastModifiedHTMLFile } from './util';
 
-test.describe('ai todo', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('https://todomvc.com/examples/react/dist/');
-  });
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://todomvc.com/examples/react/dist/');
+});
 
-  test.afterEach(async ({ page, ai, aiAssert }) => {
+test.afterEach(async ({ page, ai, aiAssert }, testInfo) => {
+  if (testInfo.title.indexOf('ai todo') !== -1) {
     const htmlFile = getLastModifiedHTMLFile(
       path.join(__dirname, '../../../midscene_run/report'),
     );
@@ -29,34 +29,34 @@ test.describe('ai todo', () => {
     await aiAssert(
       'On the left taskbar, check whether the specific execution content of the right task is normal',
     );
-  });
+  }
+});
 
-  test('ai todo', async ({ ai, aiQuery, aiWaitFor }) => {
-    await ai(
-      'Enter "Learn JS today" in the task box, then press Enter to create',
-    );
+test('ai todo', async ({ ai, aiQuery, aiWaitFor }) => {
+  await ai(
+    'Enter "Learn JS today" in the task box, then press Enter to create',
+  );
 
-    await aiWaitFor('the input box for task title is empty now');
+  await aiWaitFor('the input box for task title is empty now');
 
-    await ai(
-      'Enter "Learn Rust tomorrow" in the task box, then press Enter to create',
-    );
-    await ai(
-      'Enter "Learning AI the day after tomorrow" in the task box, then press Enter to create',
-    );
-    await ai('Move your mouse over the second item in the task list');
-    await ai('Click the delete button to the right of the second task');
-    await ai('Click the check button to the left of the second task');
-    await ai('Click the completed Status button below the task list');
+  await ai(
+    'Enter "Learn Rust tomorrow" in the task box, then press Enter to create',
+  );
+  await ai(
+    'Enter "Learning AI the day after tomorrow" in the task box, then press Enter to create',
+  );
+  await ai('Move your mouse over the second item in the task list');
+  await ai('Click the delete button to the right of the second task');
+  await ai('Click the check button to the left of the second task');
+  await ai('Click the completed Status button below the task list');
 
-    const taskList = await aiQuery<string[]>('string[], tasks in the list');
-    expect(taskList.length).toBe(1);
-    expect(taskList[0]).toBe('Learning AI the day after tomorrow');
+  const taskList = await aiQuery<string[]>('string[], tasks in the list');
+  expect(taskList.length).toBe(1);
+  expect(taskList[0]).toBe('Learning AI the day after tomorrow');
 
-    const placeholder = await ai(
-      'string, return the placeholder text in the input box',
-      { type: 'query' },
-    );
-    expect(placeholder).toBe('What needs to be done?');
-  });
+  const placeholder = await ai(
+    'string, return the placeholder text in the input box',
+    { type: 'query' },
+  );
+  expect(placeholder).toBe('What needs to be done?');
 });
