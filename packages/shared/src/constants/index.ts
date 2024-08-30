e89cfeda4a08bf68ca93eb9fd750c085b48ec616
yutao <yutao.tao@bytedance.com>
2024-08-30 10:46:53 +0800

    fix: ts type definition

--- packages/shared/src/constants/index.ts
+++ packages/shared/src/constants/index.ts
@@ -3,8 +3,8 @@ export const TEXT_SIZE_THRESHOLD = 9;
 export const TEXT_MAX_SIZE = 40;
 
 export enum NodeType {
-  CONTAINER = 'CONTAINER',
-  FORM_ITEM = 'FORM_ITEM',
+  CONTAINER = 'CONTAINER Node',
+  FORM_ITEM = 'FORM_ITEM Node',
   BUTTON = 'BUTTON Node',
   IMG = 'IMG Node',
   TEXT = 'TEXT Node',
