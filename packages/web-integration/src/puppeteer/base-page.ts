e14f29d62ef7fa0c308c812e4a88147bc17eea40
yutao <yutao.tao@bytedance.com>
2024-09-09 13:55:09 +0800

    fix: update CI snapshot

---
+++ packages/web-integration/src/puppeteer/base-page.ts
@@ -0,0 +1,107 @@
+import type { Page as PlaywrightPage } from 'playwright';
+import type { Page as PuppeteerPage } from 'puppeteer';
+import type { WebKeyInput } from '../common/page';
+import { getExtraReturnLogic } from '../common/utils';
+import type { ElementInfo } from '../extractor';
+import type { AbstractPage, screenshotOptions } from '../page';
+import type { MouseButton } from '../page';
+
+export class Page<
+  AgentType extends 'puppeteer' | 'playwright',
+  PageType extends PuppeteerPage | PlaywrightPage,
+> implements AbstractPage
+{
+  private page: PageType;
+  pageType: AgentType;
+
+  private evaluate<R>(
+    pageFunction: string | ((...args: any[]) => R | Promise<R>),
+    arg?: any,
+  ): Promise<R> {
+    if (this.pageType === 'puppeteer') {
+      return (this.page as PuppeteerPage).evaluate(pageFunction, arg);
+    }
+    return (this.page as PlaywrightPage).evaluate(pageFunction, arg);
+  }
+
+  constructor(page: PageType, pageType: AgentType) {
+    this.page = page;
+    this.pageType = pageType;
+  }
+
+  async getElementInfos() {
+    const scripts = await getExtraReturnLogic();
+    const captureElementSnapshot = await this.evaluate(scripts);
+    return captureElementSnapshot as ElementInfo[];
+  }
+
+  screenshot(options: screenshotOptions): Promise<Uint8Array> {
+    const { path } = options;
+    return this.page.screenshot({ path, type: 'png' });
+  }
+
+  url(): string {
+    return this.page.url();
+  }
+
+  get mouse() {
+    return {
+      click: async (x: number, y: number, options?: { button: MouseButton }) =>
+        this.page.mouse.click(x, y, { button: options?.button || 'left' }),
+      wheel: async (deltaX: number, deltaY: number) => {
+        if (this.pageType === 'puppeteer') {
+          await (this.page as PuppeteerPage).mouse.wheel({ deltaX, deltaY });
+        } else if (this.pageType === 'playwright') {
+          await (this.page as PlaywrightPage).mouse.wheel(deltaX, deltaY);
+        }
+      },
+      move: async (x: number, y: number) => this.page.mouse.move(x, y),
+    };
+  }
+
+  get keyboard() {
+    return {
+      type: async (text: string) => this.page.keyboard.type(text),
+      press: async (key: WebKeyInput) => this.page.keyboard.press(key),
+      down: async (key: WebKeyInput) => this.page.keyboard.down(key),
+      up: async (key: WebKeyInput) => this.page.keyboard.up(key),
+    };
+  }
+
+  async clearInput(element: ElementInfo): Promise<void> {
+    if (!element) {
+      return;
+    }
+
+    await this.mouse.click(element.center[0], element.center[1]);
+
+    const isMac = process.platform === 'darwin';
+    if (isMac) {
+      await this.page.keyboard.down('Meta');
+      await this.page.keyboard.press('a');
+      await this.page.keyboard.up('Meta');
+    } else {
+      await this.page.keyboard.down('Control');
+      await this.page.keyboard.press('a');
+      await this.page.keyboard.up('Control');
+    }
+    await this.keyboard.press('Backspace');
+  }
+
+  scrollUntilTop(): Promise<void> {
+    return this.mouse.wheel(0, -9999999);
+  }
+  scrollUntilBottom(): Promise<void> {
+    return this.mouse.wheel(0, 9999999);
+  }
+  async scrollUpOneScreen(): Promise<void> {
+    const innerHeight = await this.evaluate(() => window.innerHeight);
+    const distance = innerHeight * 0.7;
+    await this.mouse.wheel(0, -distance);
+  }
+  async scrollDownOneScreen(): Promise<void> {
+    const innerHeight = await this.evaluate(() => window.innerHeight);
+    const distance = innerHeight * 0.7;
+    await this.mouse.wheel(0, distance);
+  }
+}
