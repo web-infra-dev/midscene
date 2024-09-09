fff6c57cc5f15a29c993aca8bb8082ce9ebb6014
yutao <yutao.tao@bytedance.com>
2024-09-09 14:58:03 +0800

    fix: CI

--- packages/midscene/tests/ai/openai.test.ts
+++ packages/midscene/tests/ai/openai.test.ts
@@ -1,5 +1,5 @@
+import { AIActionType } from '@/ai-model/common';
 import { call, callToGetJSONObject } from '@/ai-model/openai';
-import { AIResponseFormat } from '@/types';
 import { describe, expect, it, vi } from 'vitest';
 
 vi.setConfig({
@@ -7,35 +7,34 @@ vi.setConfig({
 });
 describe('openai', () => {
   it('basic', async () => {
-    const result = await call(
-      [
-        {
-          role: 'system',
-          content: 'Answer the question',
-        },
-        {
-          role: 'user',
-          content:
-            '鲁迅认识周树人吗？回答我：1. 分析原因 2.回答：是/否/无效问题',
-        },
-      ],
-      AIResponseFormat.TEXT,
-    );
+    const result = await call([
+      {
+        role: 'system',
+        content: 'Answer the question',
+      },
+      {
+        role: 'user',
+        content: '鲁迅认识周树人吗？回答我：1. 分析原因 2.回答：是/否/无效问题',
+      },
+    ]);
 
     expect(result.length).toBeGreaterThan(1);
   });
 
   it('call to get json result', async () => {
-    const result = await callToGetJSONObject<{ answer: number }>([
-      {
-        role: 'system',
-        content: 'Answer the question with JSON: {answer: number}',
-      },
-      {
-        role: 'user',
-        content: '3 x 5 = ?',
-      },
-    ]);
+    const result = await callToGetJSONObject<{ answer: number }>(
+      [
+        {
+          role: 'system',
+          content: 'Answer the question with JSON: {answer: number}',
+        },
+        {
+          role: 'user',
+          content: '3 x 5 = ?',
+        },
+      ],
+      AIActionType.EXTRACT_DATA,
+    );
     expect(result.answer).toBe(15);
   });
 });
