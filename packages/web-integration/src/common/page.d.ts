dbcea923d6a6fbde167c1c7bfad652d74654eb57
yutao <yutao.tao@bytedance.com>
2024-09-09 17:01:12 +0800

    fix: CI

--- packages/web-integration/src/common/page.d.ts
+++ packages/web-integration/src/common/page.d.ts
@@ -1,7 +1,7 @@
 import type { KeyInput } from 'puppeteer';
 import type { AppiumPage } from '../appium';
 import type { PlaywrightPage } from '../playwright';
-import type { PuppeteerPage } from '../puppeteer';
+import type { PuppeteerWebPage } from '../puppeteer';
 
-export type WebPage = PlaywrightPage | PuppeteerPage | AppiumPage;
+export type WebPage = PlaywrightPage | PuppeteerWebPage | AppiumPage;
 export type WebKeyInput = KeyInput;
--- packages/web-integration/src/puppeteer/index.ts
+++ packages/web-integration/src/puppeteer/index.ts
@@ -1,2 +1,11 @@
-export { PageAgent as PuppeteerAgent } from '@/common/agent';
-export { Page as PuppeteerPage } from './page';
+import { PageAgent, type PageAgentOpt } from '@/common/agent';
+import type { Page as PuppeteerPage } from 'puppeteer';
+import { Page as PuppeteerWebPage } from './page';
+
+export { Page as PuppeteerWebPage } from './page';
+export class PuppeteerAgent extends PageAgent {
+  constructor(page: PuppeteerPage, opts?: PageAgentOpt) {
+    const webPage = new PuppeteerWebPage(page);
+    super(webPage, opts);
+  }
+}
