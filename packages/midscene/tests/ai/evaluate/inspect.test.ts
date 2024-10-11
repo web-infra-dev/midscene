e559c476bc438f5d195c39376b08f5a68a10a1b2
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2024-10-11 17:33:37 +0800

    feat(ai-model): optimize AI model and update evaluation tests

--- packages/midscene/tests/ai/evaluate/inspect.test.ts
+++ packages/midscene/tests/ai/evaluate/inspect.test.ts
@@ -119,25 +119,25 @@ describe('ai inspect element', () => {
   });
 });
 
-// test('inspect with quick answer', async () => {
-//   const { context } = await getPageTestData(
-//     path.join(__dirname, './test-data/todo'),
-//   );
+test('inspect with quick answer', async () => {
+  const { context } = await getPageTestData(
+    path.join(__dirname, './test-data/todo'),
+  );
 
-//   const startTime = Date.now();
-//   const { parseResult } = await AiInspectElement({
-//     context,
-//     multi: false,
-//     targetElementDescription: 'never mind',
-//     quickAnswer: {
-//       id: 'fbc2d0029b',
-//       nodeHashId: 'fbc2d0029b',
-//       reason: 'never mind',
-//       text: 'never mind',
-//     },
-//   });
-//   const endTime = Date.now();
-//   const cost = endTime - startTime;
-//   expect(parseResult.elements.length).toBe(1);
-//   expect(cost).toBeLessThan(100);
-// });
+  const startTime = Date.now();
+  const { parseResult } = await AiInspectElement({
+    context,
+    multi: false,
+    targetElementDescription: 'never mind',
+    quickAnswer: {
+      id: 'fbc2d0029b',
+      nodeHashId: 'fbc2d0029b',
+      reason: 'never mind',
+      text: 'never mind',
+    },
+  });
+  const endTime = Date.now();
+  const cost = endTime - startTime;
+  expect(parseResult.elements.length).toBe(1);
+  expect(cost).toBeLessThan(100);
+});
