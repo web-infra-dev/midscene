9333e63a9547cf837853d25406696fc3413ee3a5
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2024-10-28 10:39:50 +0800

    chore: delete unless fs mock file

--- packages/web-integration/src/mock_fs.ts
+++
@@ -1,8 +0,0 @@
-const throwError = () => {
-  throw new Error('this should not be called in browser');
-};
-export default {};
-export const existsSync = throwError;
-export const readFileSync = throwError;
-export const mkdirSync = throwError;
-export const writeFileSync = throwError;
