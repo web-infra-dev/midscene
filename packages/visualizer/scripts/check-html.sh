6bc8cc63f7e2df295332cf2319388de3e57c6ac0
yutao <yutao.tao@bytedance.com>
2024-11-22 11:30:36 +0800

    feat: use yaml style e2e

--- packages/visualizer/scripts/check-html.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-node ../cli/bin/midscene --serve ./dist/report --url demo.html \
-  --action "Click the 'Insight / Locate' on Left" \
-  --sleep 300 \
-  --assert "There is a 'Open in Playground' button on the page"
