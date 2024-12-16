3dfc9c80f9c0f90b2da135f20d32cdb5f6cb2935
yutao <yutao.tao@bytedance.com>
2024-12-16 10:49:59 +0800

    fix: ai test

--- packages/web-integration/tests/ai/web/puppeteer/utils.ts
+++ packages/web-integration/tests/ai/web/puppeteer/utils.ts
@@ -29,7 +29,9 @@ export async function launchPage(
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
   );
   const response = await originPage.goto(url);
-  await originPage.waitForNetworkIdle();
+  await originPage.waitForNetworkIdle({
+    concurrency: 1,
+  });
   if (response?.status) {
     assert(response.status() <= 399, `Page load failed: ${response.status()}`);
   }
