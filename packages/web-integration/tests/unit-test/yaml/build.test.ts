e5f9e43092e61670f0fb8b8b3f04ae852b4cd86b
yutao <yutao.tao@bytedance.com>
2024-12-25 12:00:29 +0800

    fix: lint

--- packages/web-integration/tests/unit-test/yaml/build.test.ts
+++
@@ -1,9 +0,0 @@
-import { buildYaml } from '@/yaml';
-import { describe, expect, test } from 'vitest';
-
-describe('utils', () => {
-  test('build yaml', () => {
-    const yaml = buildYaml({ url: 'https://www.baidu.com' }, []);
-    expect(yaml).toMatchSnapshot();
-  });
-});
