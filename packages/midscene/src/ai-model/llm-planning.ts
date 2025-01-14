077a77135eed708e873e1e48e795cd81c04c6e54
yutao <yutao.tao@bytedance.com>
2025-01-14 19:12:32 +0800

    chore: rename files

---
+++ packages/midscene/src/ai-model/llm-planning.ts
@@ -0,0 +1,69 @@
+import assert from 'node:assert';
+import type { AIUsageInfo, PlanningAIResponse, UIContext } from '@/types';
+import { PromptTemplate } from '@langchain/core/prompts';
+import { AIActionType, type AIArgs, callAiFn } from './common';
+import {
+  automationUserPrompt,
+  generateTaskBackgroundContext,
+  systemPromptToTaskPlanning,
+} from './prompt/llm-planning';
+import { describeUserPage } from './prompt/util';
+
+export async function plan(
+  userPrompt: string,
+  opts: {
+    whatHaveDone?: string;
+    originalPrompt?: string;
+    context: UIContext;
+    callAI?: typeof callAiFn<PlanningAIResponse>;
+  },
+): Promise<PlanningAIResponse> {
+  const { callAI, context } = opts || {};
+  const { screenshotBase64, screenshotBase64WithElementMarker } = context;
+  const { description: pageDescription, elementByPosition } =
+    await describeUserPage(context);
+
+  const systemPrompt = await systemPromptToTaskPlanning();
+  const taskBackgroundContextText = generateTaskBackgroundContext(
+    userPrompt,
+    opts.originalPrompt,
+    opts.whatHaveDone,
+  );
+  const userInstructionPrompt = await automationUserPrompt.format({
+    pageDescription,
+    taskBackgroundContext: taskBackgroundContextText,
+  });
+
+  const msgs: AIArgs = [
+    { role: 'system', content: systemPrompt },
+    {
+      role: 'user',
+      content: [
+        {
+          type: 'image_url',
+          image_url: {
+            url: screenshotBase64WithElementMarker || screenshotBase64,
+            detail: 'high',
+          },
+        },
+        {
+          type: 'text',
+          text: userInstructionPrompt,
+        },
+      ],
+    },
+  ];
+
+  const call = callAI || callAiFn;
+  const { content, usage } = await call(msgs, AIActionType.PLAN);
+  const planFromAI = content;
+
+  const actions = planFromAI?.actions || [];
+  assert(planFromAI, "can't get plans from AI");
+  assert(
+    actions.length > 0,
+    `Failed to plan actions: ${planFromAI.error || '(no error details)'}`,
+  );
+
+  return planFromAI;
+}
---
+++ packages/midscene/src/ai-model/prompt/llm-locator.ts
@@ -0,0 +1,205 @@
+import { MATCH_BY_POSITION, getAIConfig } from '@/env';
+import { PromptTemplate } from '@langchain/core/prompts';
+import type { ResponseFormatJSONSchema } from 'openai/resources';
+import { systemPromptToLocateElementPosition } from './ui-tars-locator';
+
+export function systemPromptToLocateElement() {
+  if (getAIConfig(MATCH_BY_POSITION)) {
+    return systemPromptToLocateElementPosition();
+  }
+  return `
+## Role:
+You are an expert in software page image (2D) and page element text analysis.
+
+## Objective:
+- Identify elements in screenshots and text that match the user's description.
+- Return JSON data containing the selection reason and element ID.
+
+## Skills:
+- Image analysis and recognition
+- Multilingual text understanding
+- Software UI design and testing
+
+## Workflow:
+1. Receive the user's element description, screenshot, and element description information. Note that the text may contain non-English characters (e.g., Chinese), indicating that the application may be non-English.
+2. Based on the user's description, locate the target element ID in the list of element descriptions and the screenshot.
+3. Found the required number of elements
+4. Return JSON data containing the selection reason and element ID.
+
+## Constraints:
+- Strictly adhere to the specified location when describing the required element; do not select elements from other locations.
+- Elements in the image with NodeType other than "TEXT Node" have been highlighted to identify the element among multiple non-text elements.
+- Accurately identify element information based on the user's description and return the corresponding element ID from the element description information, not extracted from the image.
+- If no elements are found, the "elements" array should be empty.
+- The returned data must conform to the specified JSON format.
+- The returned value id information must use the id from element info (important: **use id not indexId, id is hash content**)
+
+## Output Format:
+
+Please return the result in JSON format as follows:
+
+\`\`\`json
+{
+  "elements": [
+    // If no matching elements are found, return an empty array []
+    {
+      "reason": "PLACEHOLDER", // The thought process for finding the element, replace PLACEHOLDER with your thought process
+      "text": "PLACEHOLDER", // Replace PLACEHOLDER with the text of elementInfo, if none, leave empty
+      "id": "PLACEHOLDER" // Replace PLACEHOLDER with the ID (important: **use id not indexId, id is hash content**) of elementInfo
+    }
+    // More elements...
+  ],
+  "errors": [] // Array of strings containing any error messages
+}
+\`\`\`
+
+## Example:
+Example 1:
+Input Example:
+\`\`\`json
+// Description: "Shopping cart icon in the upper right corner"
+{
+  "description": "PLACEHOLDER", // Description of the target element
+  "screenshot": "path/screenshot.png",
+  "text": '{
+      "pageSize": {
+        "width": 400, // Width of the page
+        "height": 905 // Height of the page
+      },
+      "elementInfos": [
+        {
+          "id": "1231", // ID of the element
+          "indexId": "0", // Index of the element，The image is labeled to the left of the element
+          "attributes": { // Attributes of the element
+            "nodeType": "IMG Node", // Type of element, types include: TEXT Node, IMG Node, BUTTON Node, INPUT Node
+            "src": "https://ap-southeast-3.m",
+            "class": ".img"
+          },
+          "content": "", // Text content of the element
+          "rect": {
+            "left": 280, // Distance from the left side of the page
+            "top": 8, // Distance from the top of the page
+            "width": 44, // Width of the element
+            "height": 44 // Height of the element
+          }
+        },
+        {
+          "id": "66551", // ID of the element
+          "indexId": "1", // Index of the element,The image is labeled to the left of the element
+          "attributes": { // Attributes of the element
+            "nodeType": "IMG Node", // Type of element, types include: TEXT Node, IMG Node, BUTTON Node, INPUT Node
+            "src": "data:image/png;base64,iVBORw0KGgoAAAANSU...",
+            "class": ".icon"
+          },
+          "content": "", // Text content of the element
+          "rect": {
+            "left": 350, // Distance from the left side of the page
+            "top": 16, // Distance from the top of the page
+            "width": 25, // Width of the element
+            "height": 25 // Height of the element
+          }
+        },
+        ...
+        {
+          "id": "12344",
+          "indexId": "2", // Index of the element，The image is labeled to the left of the element
+          "attributes": {
+            "nodeType": "TEXT Node",
+            "class": ".product-name"
+          },
+          "center": [
+            288,
+            834
+          ],
+          "content": "Mango Drink",
+          "rect": {
+            "left": 188,
+            "top": 827,
+            "width": 199,
+            "height": 13
+          }
+        },
+        ...
+      ]
+    }
+  '
+}
+\`\`\`
+Output Example:
+\`\`\`json
+{
+  "elements": [
+    {
+      // Describe the reason for finding this element, replace with actual value in practice
+      "reason": "Reason for finding element 4: It is located in the upper right corner, is an image type, and according to the screenshot, it is a shopping cart icon button",
+      "text": "",
+      // ID(**use id not indexId**) of this element, replace with actual value in practice, **use id not indexId**
+      "id": "1231"
+    }
+  ],
+  "errors": []
+}
+\`\`\`
+  
+  `;
+}
+
+export const findElementSchema: ResponseFormatJSONSchema = {
+  type: 'json_schema',
+  json_schema: {
+    name: 'find_elements',
+    strict: true,
+    schema: {
+      type: 'object',
+      properties: {
+        elements: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              reason: {
+                type: 'string',
+                description: 'Reason for finding this element',
+              },
+              text: {
+                type: 'string',
+                description: 'Text content of the element',
+              },
+              id: {
+                type: 'string',
+                description: 'ID of this element',
+              },
+            },
+            required: ['reason', 'text', 'id'],
+            additionalProperties: false,
+          },
+          description: 'List of found elements',
+        },
+        errors: {
+          type: 'array',
+          items: {
+            type: 'string',
+          },
+          description: 'List of error messages, if any',
+        },
+      },
+      required: ['elements', 'errors'],
+      additionalProperties: false,
+    },
+  },
+};
+
+export const findElementPrompt = new PromptTemplate({
+  template: `
+    Here is the item user want to find. Just go ahead:
+    =====================================
+    {{
+      "description": "{targetElementDescription}",
+      "multi": {multi}
+    }}
+    =====================================
+
+    pageDescription: {pageDescription}
+  `,
+  inputVariables: ['pageDescription', 'targetElementDescription', 'multi'],
+});
