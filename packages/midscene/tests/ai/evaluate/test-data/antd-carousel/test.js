067cc319f1529758e6e283bcd6bfc2e186237a4f
yutao <yutao.tao@bytedance.com>
2025-02-06 19:12:47 +0800

    chore: remove unused file

--- packages/midscene/tests/ai/evaluate/test-data/antd-carousel/test.js
+++
@@ -1,7 +0,0 @@
-const readFile = require('fs').readFileSync;
-const path = require('path');
-
-const file = readFile(path.join(__dirname, 'element-tree.txt'), 'utf8');
-
-const data = file.replace(/^\s*\n/gm, '');
-console.log(data);
