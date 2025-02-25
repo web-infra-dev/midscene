111cf25d31b1b6c54fc4dd8fbead2aac2a5e329d
Zhaopeng XUAN <xuanzhaopeng@gmail.com>
2025-02-25 07:49:14 +0100

    feat(core/ui-tars): support aiAssert() & aiWaitFor for UI-Tars model (#412)
    * Issue-41 Support aiAssert() for ui-tars model
    
    ---------
    Co-authored-by: zhouxiao.shaw <zhouxiao.shaw@bytedance.com>

--- packages/midscene/tests/ai/connectivity.test.ts
+++ packages/midscene/tests/ai/connectivity.test.ts
@@ -22,11 +22,14 @@ vi.setConfig({
   '.env.init_json',
   '.env.anthropic',
   '.env.openai',
+  '.env.ui-tars',
 ].forEach((envFile) => {
   const configPath = path.resolve(__dirname, `../../${envFile}`);
   if (!existsSync(configPath)) {
     return;
   }
+
+  const isUiTars = envFile === '.env.ui-tars';
 
   describe(`LLM service connectivity: ${envFile}`, () => {
     beforeAll(() => {
@@ -41,22 +44,25 @@ vi.setConfig({
     });
 
     it('text only', async () => {
-      const result = await call([
-        {
-          role: 'system',
-          content: 'Answer the question',
-        },
-        {
-          role: 'user',
-          content:
-            '鲁迅认识周树人吗？回答我：1. 分析原因 2.回答：是/否/无效问题',
-        },
-      ]);
+      const result = await call(
+        [
+          {
+            role: 'system',
+            content: 'Answer the question',
+          },
+          {
+            role: 'user',
+            content:
+              '鲁迅认识周树人吗？回答我：1. 分析原因 2.回答：是/否/无效问题',
+          },
+        ],
+        AIActionType.EXTRACT_DATA,
+      );
 
       expect(result.content.length).toBeGreaterThan(1);
     });
 
-    it('call to get json result', async () => {
+    it.skipIf(isUiTars)('call to get json result', async () => {
       const result = await callToGetJSONObject<{ answer: number }>(
         [
           {
@@ -68,31 +74,62 @@ vi.setConfig({
             content: '3 x 5 = ?',
           },
         ],
-        AIActionType.EXTRACT_DATA,
+        AIActionType.INSPECT_ELEMENT,
+      );
+      expect(result.content).toEqual({ answer: 15 });
+    });
+
+    it.skipIf(!isUiTars)('for ui-tars, call to get json result', async () => {
+      const result = await callToGetJSONObject<{ answer: number }>(
+        [
+          {
+            role: 'system',
+            content: `Answer the question
+## Output Json String Format
+\`\`\`
+"{
+   "answer": <<is a number, the answer of the question>>, 
+}"
+
+## Rules **MUST** follow
+- Make sure to return **only** the JSON, with **no additional** text or explanations.
+- You **MUST** strict follow up the **Output Json String Format**.
+\`\`\`
+`,
+          },
+          {
+            role: 'user',
+            content: '3 x 5 = ?',
+          },
+        ],
+        AIActionType.INSPECT_ELEMENT,
       );
       expect(result.content).toEqual({ answer: 15 });
     });
 
     it('image input', async () => {
       const imagePath = getFixture('baidu.png');
-      const result = await call([
-        {
-          role: 'user',
-          content: [
-            {
-              type: 'text',
-              text: 'Describe this image in one sentence.',
-            },
-            {
-              type: 'image_url',
-              image_url: {
-                url: base64Encoded(imagePath),
-                detail: 'high',
+      const result = await call(
+        [
+          {
+            role: 'user',
+            content: [
+              {
+                type: 'text',
+                text: 'Describe this image in one sentence.',
               },
-            },
-          ],
-        },
-      ]);
+              {
+                type: 'image_url',
+                image_url: {
+                  url: base64Encoded(imagePath),
+                  detail: 'high',
+                },
+              },
+            ],
+          },
+        ],
+        AIActionType.EXTRACT_DATA,
+      );
 
       expect(result.content.length).toBeGreaterThan(10);
     });
