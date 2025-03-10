ef81226e1af0328fa7919fc382b20562a3e88438
yutao <yutao.tao@bytedance.com>
2025-03-10 13:51:17 +0800

    fix: ai e2e

--- packages/web-integration/tests/playwright.config.ts
+++ packages/web-integration/tests/playwright.config.ts
@@ -1,12 +1,12 @@
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+// import { fileURLToPath } from 'node:url';
 import { defineConfig, devices } from '@playwright/test';
 //@ts-ignore
 import dotenv from 'dotenv';
 
 // 添加这两行获取当前文件目录（ES 模块兼容方式）
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const MIDSCENE_REPORT = process.env.MIDSCENE_REPORT;
 
