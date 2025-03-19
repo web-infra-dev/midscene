539748b006c529f32b2c9f1d6f9cb56563c13ab3
yutao <yutao.tao@bytedance.com>
2025-03-19 13:51:51 +0800

    chore: add yaml support for aiTap

--- packages/core/tests/ai/insight/insight.test.ts
+++ packages/core/tests/ai/insight/insight.test.ts
@@ -1,7 +1,6 @@
 import Insight from '@/insight';
 import { sleep } from '@/utils';
 import { getContextFromFixture } from 'tests/evaluation';
-<<<<<<< HEAD
 import { expect, test, vi } from 'vitest';
 
 vi.setConfig({
@@ -32,20 +31,15 @@ test('insight locate with search area and think twice', async () => {
   expect(element).toBeDefined();
   await sleep(3000);
 });
-=======
-import { expect, test } from 'vitest';
+
+vi.setConfig({
+  testTimeout: 60 * 1000,
+});
 
 test(
   'insight locate with search area',
   async () => {
     const { context } = await getContextFromFixture('taobao');
-
-    const insight = new Insight(context);
-    const { element } = await insight.locate({
-      prompt: '购物车 icon',
-      searchArea: '顶部购物车栏目',
-    });
-    expect(element).toBeDefined();
 
     await sleep(3000);
   },
@@ -53,4 +47,3 @@ test(
     timeout: 60 * 1000,
   },
 );
->>>>>>> ba96da7 (feat: enable search area for locate)
