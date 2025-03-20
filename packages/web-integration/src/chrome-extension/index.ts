2d8db57f820e39973df3746ccbbcade5832ea777
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2025-03-20 15:59:54 +0800

    feat: export overrideAIConfig in chrome extension

--- packages/web-integration/src/chrome-extension/index.ts
+++ packages/web-integration/src/chrome-extension/index.ts
@@ -2,6 +2,8 @@ import { ERROR_CODE_NOT_IMPLEMENTED_AS_DESIGNED } from '../common/utils';
 import { ChromeExtensionProxyPageAgent } from './agent';
 import ChromeExtensionProxyPage from './page';
 
+export { overrideAIConfig } from '@midscene/core/env';
+
 export {
   ChromeExtensionProxyPage,
   ChromeExtensionProxyPageAgent,
