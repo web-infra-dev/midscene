0642e300de7d0e65a449b7e01de0412845bf03fe
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2025-04-01 18:45:38 +0800

    chore: optimize common logic

--- packages/visualizer/src/extension.tsx
+++ packages/visualizer/src/extension.tsx
@@ -1,5 +1,5 @@
 export { Logo } from './component/logo';
-export { Playground } from './component/playground-component';
+export { Playground } from './component/playground/index';
 export { globalThemeConfig } from './component/color';
 export { useEnvConfig } from './component/store';
 
