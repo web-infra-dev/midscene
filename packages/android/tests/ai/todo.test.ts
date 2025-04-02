3716bacb701146fff5c2e8f2b7626c59e7f3fbf5
quanruzhuoxiu <quanruzhuoxiu@gmail.com>
2025-04-02 21:01:39 +0800

    fix: click before clearInput

--- packages/android/tests/ai/todo.test.ts
+++ packages/android/tests/ai/todo.test.ts
@@ -1,7 +1,6 @@
 import { sleep } from '@midscene/core/utils';
 import { beforeAll, describe, expect, it, vi } from 'vitest';
 import { AndroidAgent, AndroidDevice, getConnectedDevices } from '../../src';
-import { sleep } from '@midscene/core/utils';
 
 vi.setConfig({
   testTimeout: 240 * 1000,
