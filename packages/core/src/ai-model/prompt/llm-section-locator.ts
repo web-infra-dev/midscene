a6ac78aee3cea2151af87f2673f60ff443fe8e92
yuyutaotao <167746126+yuyutaotao@users.noreply.github.com>
2025-04-18 13:35:58 +0800

    chore(core): update prompt for section locator (#588)

--- packages/core/src/ai-model/prompt/llm-section-locator.ts
+++ packages/core/src/ai-model/prompt/llm-section-locator.ts
@@ -4,8 +4,6 @@ export function systemPromptToLocateSection() {
   return `
 You goal is to find out one section containing the target element in the screenshot, put it in the \`bbox\` field. If the user describe the target element with some reference elements, you should also find the section containing the reference elements, put it in the \`references_bbox\` field.
 
-For example, if the user describe the target element as "the delete button on the second row with title 'Peter'", you should put the bounding box of the delete button in the \`bbox\` field, and the bounding box of the second row in the \`references_bbox\` field.
-
 Usually, it should be approximately an area not more than 300x300px. Changes of the size are allowed if there are many elements to cover.
 
 return in this JSON format:
@@ -22,6 +20,16 @@ return in this JSON format:
 \`\`\`
 
 In which, all the numbers in the \`bbox\` means the distance to the left, top, right, bottom of the page.
+
+For example, if the user describe the target element as "the delete button on the second row with title 'Peter'", you should put the bounding box of the delete button in the \`bbox\` field, and the bounding box of the second row in the \`references_bbox\` field.
+
+the return value should be like this:
+\`\`\`json
+{
+  "bbox": [100, 100, 200, 200],
+  "references_bbox": [[100, 100, 200, 200]]
+}
+\`\`\`
 `;
 }
 
