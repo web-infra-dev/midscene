791f0f03622d3ac67f944ec6b0394d09cd324063
quanruzhuoxiu <quanruzhuoxiu@gmail.com>
2025-04-22 14:36:45 +0800

    style(chrome-extension): opt chrome logo icon

--- packages/visualizer/src/component/logo.tsx
+++ packages/visualizer/src/component/logo.tsx
@@ -3,39 +3,19 @@ import './logo.less';
 export const LogoUrl =
   'https://lf3-static.bytednsdoc.com/obj/eden-cn/vhaeh7vhabf/Midscene.png';
 
-export const Logo = ({
-  withGithubStar = false,
-  hideLogo = false,
-}: { withGithubStar?: boolean; hideLogo?: boolean }) => {
+export const Logo = ({ hideLogo = false }: { hideLogo?: boolean }) => {
   if (hideLogo) {
     return null;
   }
 
-  if (withGithubStar) {
-    return (
-      <div className="logo logo-with-star-wrapper">
-        <img alt="Midscene_logo" src={LogoUrl} />
-        <a
-          href="https://github.com/web-infra-dev/midscene"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="github-star"
-            src="https://img.shields.io/github/stars/web-infra-dev/midscene?style=social"
-            alt="Github star"
-          />
-        </a>
-      </div>
-    );
-  }
-
   return (
     <div className="logo">
-      <img
-        alt="Midscene_logo"
-        src="https://lf3-static.bytednsdoc.com/obj/eden-cn/vhaeh7vhabf/Midscene.png"
-      />
+      <a href="https://midscenejs.com/" target="_blank" rel="noreferrer">
+        <img
+          alt="Midscene_logo"
+          src="https://lf3-static.bytednsdoc.com/obj/eden-cn/vhaeh7vhabf/Midscene.png"
+        />
+      </a>
     </div>
   );
 };
