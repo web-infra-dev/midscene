de5a84e6d248333b6303c4aa708a15bb8e5aedf4
quanruzhuoxiu <quanruzhuoxiu@gmail.com>
2025-05-30 11:08:12 +0800

    test(web-integration): fix tests

--- packages/core/tests/unit-test/prompt/utils.test.ts
+++ packages/core/tests/unit-test/prompt/utils.test.ts
@@ -4,11 +4,11 @@ import { describe, expect, it } from 'vitest';
 
 describe('prompt utils', () => {
   let lengthOfDescription: number;
-  it('describe context, length = 50 ', async () => {
-    const context = await getContextFromFixture('todo');
+  it('describe context ', async () => {
+    const context = await getContextFromFixture('taobao');
     const { description } = await describeUserPage(context.context, {
-      truncateTextLength: 50,
       domIncluded: true,
+      visibleOnly: false,
     });
 
     lengthOfDescription = description.length;
@@ -18,20 +18,20 @@ describe('prompt utils', () => {
     expect(stringLengthOfEachItem).toBeLessThan(250);
   });
 
-  it('describe context, length = 100, filterNonTextContent = true', async () => {
-    const context = await getContextFromFixture('todo');
+  it('describe context, truncateTextLength = 100, filterNonTextContent = true', async () => {
+    const context = await getContextFromFixture('taobao');
 
     const { description } = await describeUserPage(context.context, {
       truncateTextLength: 100,
       filterNonTextContent: true,
       domIncluded: true,
+      visibleOnly: false,
     });
 
     const stringLengthOfEachItem =
       description.length / context.context.content.length;
     expect(description).toBeTruthy();
     expect(stringLengthOfEachItem).toBeLessThan(160);
-
-    expect(description.length).toBeLessThan(lengthOfDescription);
+    expect(description.length).toBeLessThan(lengthOfDescription * 0.8);
   });
 });
