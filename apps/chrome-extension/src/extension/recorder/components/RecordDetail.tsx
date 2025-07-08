9019705b27f88d5ca5bc3e3cf7ac36f5272b989f
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2025-07-08 17:36:56 +0800

    chore: fix error formt

--- apps/chrome-extension/src/extension/recorder/components/RecordDetail.tsx
+++ apps/chrome-extension/src/extension/recorder/components/RecordDetail.tsx
@@ -211,10 +211,11 @@ export const RecordDetail: React.FC<RecordDetailProps> = ({
 
           <button
             type="button"
-            className={`flex items-center justify-center gap-1.5 flex-1 transition-colors !font-medium !leading-[1.83em] !bg-transparent !rounded-lg !py-2 !px-0 !border-none !cursor-pointer ${tab === 'code'
+            className={`flex items-center justify-center gap-1.5 flex-1 transition-colors !font-medium !leading-[1.83em] !bg-transparent !rounded-lg !py-2 !px-0 !border-none !cursor-pointer ${
+              tab === 'code'
                 ? 'text-[rgba(0,0,0,0.85)]'
                 : 'text-[rgba(0,0,0,0.25)]'
-              } ${events.length === 0 ? '!text-gray-300 !cursor-not-allowed' : ''}`}
+            } ${events.length === 0 ? '!text-gray-300 !cursor-not-allowed' : ''}`}
             style={{
               fontFamily: 'Inter, -apple-system, sans-serif',
             }}
