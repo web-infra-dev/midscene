8f6181fccebeff810437c6fbd0d784c0de9b29fc
EAGzzyCSL <eagzzycsl@hotmail.com>
2025-07-30 09:59:36 +0800

    chore(workflow): use local report template when dev (#958)
    * chore(workflow): config ts default formatter
    
    * chore(workflow): enable generate report when dev

--- packages/core/modern.config.ts
+++ packages/core/modern.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import { defineConfig, moduleTools } from '@modern-js/module-tools';
 import { version } from './package.json';
 
@@ -15,6 +16,9 @@ export default defineConfig({
     target: 'es2020',
     define: {
       __VERSION__: version,
+      __DEV_REPORT_PATH__: process.env.USE_DEV_REPORT
+        ? path.resolve(__dirname, '../../apps/report/dist/index.html')
+        : '',
     },
     splitting: true,
     sourceMap: true,
