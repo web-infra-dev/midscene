9d4f7042988ade0d5885a97fdc5cce1f98efc176
yutao <yutao.tao@bytedance.com>
2025-07-31 18:11:03 +0800

    feat(core): update jimp

--- packages/shared/src/img/get-jimp.ts
+++ packages/shared/src/img/get-jimp.ts
@@ -1,12 +1,12 @@
 // @ts-ignore
-import Jimp from 'jimp/es';
+import Jimp from 'jimp';
 import { ifInBrowser, ifInWorker } from '../utils';
 
 export default async function getJimp(): Promise<typeof Jimp> {
-  if (ifInBrowser) {
+  if (ifInBrowser || ifInWorker) {
     // @ts-ignore
     await import('jimp/browser/lib/jimp.js');
-    return (window as any).Jimp;
+    return (typeof window !== 'undefined' ? window : (self as any)).Jimp;
   }
   return Jimp;
 }
