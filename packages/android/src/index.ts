6f82157251c9842518d61529339d062ca612a9dd
yutao <yutao.tao@bytedance.com>
2025-08-25 19:54:25 +0800

    fix(core): fix ci

--- packages/android/src/index.ts
+++ packages/android/src/index.ts
@@ -1,3 +1,4 @@
 export { AndroidDevice } from './device';
-export { AndroidAgent, agentFromAdbDevice, getConnectedDevices } from './agent';
+export { AndroidAgent, agentFromAdbDevice } from './agent';
 export { overrideAIConfig } from '@midscene/shared/env';
+export { getConnectedDevices } from './utils';
