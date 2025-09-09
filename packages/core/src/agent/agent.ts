3fdb2bb98001c362f6a8d5dfb71117794af3429f
yutao <yutao.tao@bytedance.com>
2025-09-09 13:45:48 +0800

    docs(core): update docs for setAIActionContext

--- packages/core/src/agent/agent.ts
+++ packages/core/src/agent/agent.ts
@@ -204,6 +204,11 @@ export class Agent<
   }
 
   async setAIActionContext(prompt: string) {
+    if (this.opts.aiActionContext) {
+      console.warn(
+        'aiActionContext is already set, and it is called again, will override the previous setting',
+      );
+    }
     this.opts.aiActionContext = prompt;
   }
 
@@ -878,7 +883,7 @@ export class Agent<
   async runYaml(yamlScriptContent: string): Promise<{
     result: Record<string, any>;
   }> {
-    const script = parseYamlScript(yamlScriptContent, 'yaml', true);
+    const script = parseYamlScript(yamlScriptContent, 'yaml');
     const player = new ScriptPlayer(script, async () => {
       return { agent: this, freeFn: [] };
     });
