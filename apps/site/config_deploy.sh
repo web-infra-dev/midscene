9f45e1db35e17b37b75308b4fa9398ff6835ca64
yutao <yutao.tao@bytedance.com>
2025-09-23 10:37:49 +0800

    docs(core): update doc of cache

--- apps/site/config_deploy.sh
+++ apps/site/config_deploy.sh
@@ -2,10 +2,10 @@
 
 cat <<'HEADERS' > ./doc_build/_headers
 /*.js
-  Cache-Control: public, max-age=31536000, immutable
+  Cache-Control: public, max-age=86400, must-revalidate
 
 /*.css
-  Cache-Control: public, max-age=31536000, immutable
+  Cache-Control: public, max-age=86400, must-revalidate
 
 /*
   Cache-Control: public, max-age=1800, must-revalidate
