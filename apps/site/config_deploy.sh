bb1fc38ed0f2a2109ccaa6b614a707456325df89
yutao <yutao.tao@bytedance.com>
2025-09-23 10:15:15 +0800

    docs(core): set cache polify for deploy

--- apps/site/config_deploy.sh
+++ apps/site/config_deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo '/*
-  Cache-Control: public, max-age=600, must-revalidate
-' > ./doc_build/_headers
+cat <<'HEADERS' > ./doc_build/_headers
+/*.js
+  Cache-Control: public, max-age=31536000, immutable
+
+/*.css
+  Cache-Control: public, max-age=31536000, immutable
+
+/*
+  Cache-Control: public, max-age=1800, must-revalidate
+HEADERS
