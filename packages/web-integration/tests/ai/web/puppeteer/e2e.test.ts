9e17b5b6b5f105b4880810b1555b5210165d2be6
yutao <yutao.tao@bytedance.com>
2025-09-26 15:37:12 +0800

    docs(core): update changelog

--- packages/web-integration/tests/ai/web/puppeteer/e2e.test.ts
+++ packages/web-integration/tests/ai/web/puppeteer/e2e.test.ts
@@ -40,7 +40,7 @@ describe(
       }).rejects.toThrowError();
     });
 
-    it('Sauce Demo by Swag Lab', async () => {
+    it.only('Sauce Demo by Swag Lab', async () => {
       const { originPage, reset } = await launchPage(
         'https://www.saucedemo.com/',
       );
