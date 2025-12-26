c96b7bf500a4f3b584c0507a100314ea34ec9062
Frank <spadesge@gmail.com>
2025-12-26 16:02:19 +0800

    feat(benchmarks): introduce bench infra package

---
+++ benchmarks/infra/README.md
@@ -0,0 +1,15 @@
+## Midscene Benchmark Infrastructure
+
+Run [Android World benchmark](https://github.com/google-research/android_world) using Midscene.
+
+
+## Contents
+
+This package contains:
+
+* RPC Service for Midscene
+
+
+## License
+
+Midscene is MIT licensed.
---
+++ benchmarks/infra/src/types/rpc/method.ts
@@ -0,0 +1,74 @@
+/**
+ * All support rpc methods
+ */
+export enum RPCMethod {
+  NewAgent = 'new-agent',
+  RunAIMethod = 'run-ai-method',
+  TerminateAgent = 'terminate-agent',
+}
+
+/**
+ * Common Params for all rpc methods
+ */
+
+export type CommonParams<T> = {
+  id: string;
+} & T;
+
+/**
+ * Common Response for all rpc methods
+ */
+
+export type RPCResponse4Succeed<T> = {
+  code: 1;
+  data: T;
+};
+
+export type RPCResponse4Failed = {
+  code: 0;
+  data: {
+    reason: string;
+  };
+};
+
+export type RPCResponse<T> = RPCResponse4Succeed<T> | RPCResponse4Failed;
+
+/**
+ * Params for NewAgent method
+ */
+export type NewAgentParams4Android = {
+  type: 'Android';
+  deviceId: string;
+};
+
+export type NewAgentParams4IOS = {
+  type: 'iOS';
+};
+
+export type NewAgentParams4Common<T> = CommonParams<T>;
+
+export type NewAgentParams = NewAgentParams4Common<
+  NewAgentParams4Android | NewAgentParams4IOS
+>;
+
+/**
+ * Params and Response for RunAIMethod method
+ */
+
+export type RunAIMethodParams = CommonParams<{
+  task: string;
+}>;
+
+export type RunAIMethodResponse = RPCResponse<{
+  report: string;
+}>;
+
+/**
+ * Params and Response for TerminateAgent method
+ */
+
+export type TerminateAgentParams = CommonParams<{
+  userTaskStatus?: 'Successful' | 'Failed';
+}>;
+
+export type TerminateAgentResponse = RPCResponse<string>;
