2a38fd57fb27cb906359cc7eaf5ef68407adfe21
quanruzhuoxiu <quanruzhuoxiu@gmail.com>
2026-03-10 11:45:15 +0800

    fix(ios): streamline Terminate action definition in createPlatformActions

--- packages/ios/src/device.ts
+++ packages/ios/src/device.ts
@@ -1063,11 +1063,7 @@ const createPlatformActions = (device: IOSDevice) => {
         await device.launch(param);
       },
     }),
-    Terminate: defineAction<
-      typeof terminateParamSchema,
-      TerminateParam,
-      void
-    >({
+    Terminate: defineAction<typeof terminateParamSchema, TerminateParam, void>({
       name: 'Terminate',
       description: 'Terminate (close) an iOS app by its bundle ID',
       interfaceAlias: 'terminate',
