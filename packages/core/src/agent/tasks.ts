11bc646884c190ae29c99206d99800420246f9b7
xianglongfei <xianglongfei@uniontech.com>
2026-03-19 20:09:27 +0800

    fix(aiNumber):To ensure proper matching with the structure returned by the AI.

--- packages/core/src/agent/tasks.ts
+++ packages/core/src/agent/tasks.ts
@@ -594,6 +594,7 @@ export class TaskExecutor {
             [keyOfResult]: booleanPrompt,
           };
         } else if (ifTypeRestricted) {
+          keyOfResult = type;
           demandInput = {
             [keyOfResult]: `${type}, ${demand}`,
           };
