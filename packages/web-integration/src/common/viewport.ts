457a092fe58d1e35afb614d49bc04fce81fff0b7
ottomao <ottomao@gmail.com>
2026-04-29 20:24:07 +0800

    fix(web-integration): keep viewport defaults stable

--- packages/web-integration/src/common/viewport.ts
+++ packages/web-integration/src/common/viewport.ts
@@ -5,8 +5,8 @@ export interface ViewportSize {
   height: number;
 }
 
-export const defaultViewportWidth = 1400;
-export const defaultViewportHeight = 900;
+export const defaultViewportWidth = 1440;
+export const defaultViewportHeight = 768;
 export const defaultViewportSize: ViewportSize = {
   width: defaultViewportWidth,
   height: defaultViewportHeight,
