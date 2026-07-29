53da1488bdd95ab1d601939bf9c41aa766470a60
EAGzzyCSL <eagzzycsl@hotmail.com>
2026-07-29 14:29:59 +0800

    docs(site): unify model configuration guides (#2824)

--- apps/site/theme/index.tsx
+++ apps/site/theme/index.tsx
@@ -1,5 +1,6 @@
 import { BenchmarkReportPreview } from './components/BenchmarkReportPreview';
+import { ModelConfigTab, ModelConfigTabs } from './components/ModelConfigTabs';
 import { HomeLayout } from './pages';
 
-export { BenchmarkReportPreview, HomeLayout };
+export { BenchmarkReportPreview, HomeLayout, ModelConfigTab, ModelConfigTabs };
 export * from '@rspress/core/theme-original';
