f2579486bebac291c5e2c8c364e19403fa2c081a
EAGzzyCSL <eagzzycsl@hotmail.com>
2026-08-14 22:24:50 +0800

    docs(site): improve Chrome Web Store install button (#2977)

--- apps/site/theme/index.tsx
+++ apps/site/theme/index.tsx
@@ -1,6 +1,13 @@
 import { BenchmarkReportPreview } from './components/BenchmarkReportPreview';
+import { ChromeExtensionButton } from './components/ChromeExtensionButton';
 import { ModelConfigTab, ModelConfigTabs } from './components/ModelConfigTabs';
 import { HomeLayout } from './pages';
 
-export { BenchmarkReportPreview, HomeLayout, ModelConfigTab, ModelConfigTabs };
+export {
+  BenchmarkReportPreview,
+  ChromeExtensionButton,
+  HomeLayout,
+  ModelConfigTab,
+  ModelConfigTabs,
+};
 export * from '@rspress/core/theme-original';
